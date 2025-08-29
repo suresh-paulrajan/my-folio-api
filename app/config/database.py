f03dff69bf87960e789197b879498d346b92a7a0
Suresh Paulrajan <sureshpaulrajan@gmail.com>
2025-08-30 01:47:26 +0530

    PROD DB Config

--- app/config/database.py
+++ app/config/database.py
@@ -5,10 +5,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "P%40ssw0rd")
-DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
-DB_NAME = os.getenv("DB_NAME", "myfolio")
+# Local Config
+# DB_USER = os.getenv("DB_USER", "root")
+# DB_PASSWORD = os.getenv("DB_PASSWORD", "P%40ssw0rd")
+# DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+# DB_NAME = os.getenv("DB_NAME", "myfolio")
+
+# PROD Config
+DB_USER = os.getenv("DB_USER", "catalvka_bizbot_poc_dba")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "BahV3s#7^Q&E")
+DB_HOST = os.getenv("DB_HOST", "162.241.123.136")
+DB_NAME = os.getenv("DB_NAME", "catalvka_bizbot_poc")
 
 DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 

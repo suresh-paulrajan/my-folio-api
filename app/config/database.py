e3cda19e5f5e9b91b2a91b90351139c4ef6164ab
Suresh Paulrajan <sureshpaulrajan@gmail.com>
2025-08-30 13:19:09 +0530

    Externalize env config

--- app/config/database.py
+++ app/config/database.py
@@ -3,19 +3,19 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 
-# Local Config
-# DB_USER = os.getenv("DB_USER", "root")
-# DB_PASSWORD = os.getenv("DB_PASSWORD", "P%40ssw0rd")
-# DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
-# DB_NAME = os.getenv("DB_NAME", "myfolio")
+# Load environment-specific .env file
+env = os.getenv("ENV", "local")
+if env == "prod":
+	load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "../../.env.prod"))
+else:
+	load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "../../.env.local"))
 
-# PROD Config
-DB_USER = os.getenv("DB_USER", "catalvka_bizbot_poc_dba")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "BahV3s#7^Q&E")
-DB_HOST = os.getenv("DB_HOST", "162.241.123.136")
-DB_NAME = os.getenv("DB_NAME", "catalvka_bizbot_poc")
+# Get DB config from environment variables
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
 
 DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 

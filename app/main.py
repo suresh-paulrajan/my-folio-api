aff3514811873da68b1059f06e24430aa7a156be
Suresh Paulrajan <sureshpaulrajan@gmail.com>
2025-09-01 15:22:30 +0530

    Added CORS Settings

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.controllers import users_controller, auth_controller
 from app.config.database import Base, engine
 
@@ -10,3 +11,17 @@ app = FastAPI(title="myFolio API")
 # Routers
 app.include_router(users_controller.router)
 app.include_router(auth_controller.router)
+
+# CORS
+origins = [
+    "http://localhost:4200",
+    "https://myfolio.catalytix.in"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)

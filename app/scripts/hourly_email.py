42b6ba547cc8c9a06b0b7a4763655a0b0be7ad14
Suresh Paulrajan <sureshpaulrajan@gmail.com>
2025-09-09 15:11:41 +0530

    Added exception handling for Scheduled Email Job

--- app/scripts/hourly_email.py
+++ app/scripts/hourly_email.py
@@ -1,13 +1,20 @@
 import requests
+import sys
 
 # Call your own API to trigger email
-response = requests.post(
-    "https://my-folio-api.onrender.com/email/send-poc",
-    json={
-        "subject": "Hourly Update",
-        "body": "<p>This is the scheduled hourly mail.</p>",
-        "to": ["sureshpaulrajan@gmail.com"],
-        "cc": ["sureshkanna.ece@gmail.com","poc@myfolio.catalytix.in"]
-    }
-)
-print("Response:", response.json())
+try:
+    response = requests.post(
+        "https://my-folio-api.onrender.com/email/send-poc",
+        json={
+            "subject": "Hourly Update",
+            "body": "<p>This is the scheduled hourly mail.</p>",
+            "to": ["sureshpaulrajan@gmail.com"],
+            "cc": ["sureshkanna.ece@gmail.com","poc@myfolio.catalytix.in"]
+        },
+        timeout=15
+    )
+    print("Response:", response.json())
+    sys.exit(0)
+except Exception as e:
+    print("Error:", str(e))
+    sys.exit(1)
